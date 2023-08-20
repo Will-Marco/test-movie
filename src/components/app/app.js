72d4062bc4d0ac14809ef08987b21ec12050f5d3
Firdavs <mr.marco0101@gmail.com>
2023-08-20 13:26:29 +0500

    set movie-list button's functions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import AppFilter from "../app-filter/app-filter";
 import AppInfo from "../app-info/app-info";
 import MovieList from "../movie-list/movie-list";
@@ -5,46 +6,58 @@ import MoviesAddForm from "../movies-add-form/movies-add-form";
 import SearchPanel from "../search-panel/search-panel";
 import "./app.css";
 
-function App() {
-  const data = [
-    {
-      id: 1,
-      name: "Interstellar",
-      viewers: "1964004",
-      favourite: true,
-    },
-    {
-      id: 2,
-      name: "Incaption",
-      viewers: "2450544",
-      favourite: false,
-    },
-    {
-      id: 3,
-      name: "Tenet",
-      viewers: "551000",
-      favourite: false,
-    },
-    {
-      id: 4,
-      name: "Oppenheimer",
-      viewers: "358000",
-      favourite: true,
-    },
-  ];
-  return (
-    <div className="app font-monospace">
-      <div className="content">
-        <AppInfo />
-        <div className="search-panel">
-          <SearchPanel />
-          <AppFilter />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: "Interstellar",
+          viewers: "1964004",
+          favourite: true,
+        },
+        {
+          id: 2,
+          name: "Incaption",
+          viewers: "2450544",
+          favourite: false,
+        },
+        {
+          id: 3,
+          name: "Tenet",
+          viewers: "551000",
+          favourite: false,
+        },
+        {
+          id: 4,
+          name: "Oppenheimer",
+          viewers: "358000",
+          favourite: true,
+        },
+      ],
+    };
+  }
+
+  onDelete = (id) => {
+    console.log(id);
+  };
+
+  render() {
+    return (
+      <div className="app font-monospace">
+        <div className="content">
+          <AppInfo />
+          <div className="search-panel">
+            <SearchPanel />
+            <AppFilter />
+          </div>
+          <MovieList data={this.state.data} onDelete={this.onDelete} />
+          <MoviesAddForm />
         </div>
-        <MovieList data={data} />
-        <MoviesAddForm />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/movie-list/movie-list.js
+++ src/components/movie-list/movie-list.js
@@ -1,11 +1,11 @@
 import MovieListItem from "../movie-list-item/movie-list-item";
 import "./movie-list.css";
 
-const MovieList = ({ data }) => {
+const MovieList = ({ data, onDelete }) => {
   return (
     <ul className="movie-list list-group">
       {data.map((item) => (
-        <MovieListItem key={item.id} {...item} />
+        <MovieListItem key={item.id} {...item} onDelete={() => onDelete(item.id)} />
       ))}
     </ul>
   );
