7c966f58f2aada4ea89c3214fcedc4d215c70d6e
Firdavs <mr.marco0101@gmail.com>
2023-08-27 10:33:12 +0500

    convert class cmpt to function cmpt

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AppFilter from "../app-filter/app-filter";
 import AppInfo from "../app-info/app-info";
@@ -7,128 +7,106 @@ import MoviesAddForm from "../movies-add-form/movies-add-form";
 import SearchPanel from "../search-panel/search-panel";
 import "./app.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        {
-          id: uuidv4(),
-          name: "Interstellar",
-          viewers: "1964004",
-          favourite: false,
-          like: false,
-        },
-        {
-          id: uuidv4(),
-          name: "Incaption",
-          viewers: "2450544",
-          favourite: false,
-          like: false,
-        },
-        {
-          id: uuidv4(),
-          name: "Tenet",
-          viewers: "551000",
-          favourite: false,
-          like: false,
-        },
-        {
-          id: uuidv4(),
-          name: "Oppenheimer",
-          viewers: "358000",
-          favourite: false,
-          like: false,
-        },
-      ],
-      term: "",
-      filter: "all",
-    };
-  }
+function App() {
+  const [data, setData] = useState([
+    {
+      id: uuidv4(),
+      name: "Interstellar",
+      viewers: "1964004",
+      favourite: false,
+      like: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Incaption",
+      viewers: "2450544",
+      favourite: false,
+      like: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Tenet",
+      viewers: "551000",
+      favourite: false,
+      like: false,
+    },
+    {
+      id: uuidv4(),
+      name: "Oppenheimer",
+      viewers: "358000",
+      favourite: false,
+      like: false,
+    },
+  ]);
+  const [term, setTerm] = useState("");
+  const [filter, setFilter] = useState("all");
 
-  onDelete = (id) => {
-    this.setState(({ data }) => ({
-      // const idx = data.findIndex((c) => c.id === id);
-      // data.slice(idx, 1); //mutabel
-      data: data.filter((c) => c.id !== id), //immutable
-    }));
+  const onDelete = (id) => {
+    setData(data.filter((c) => c.id !== id));
   };
 
-  addForm = (item) => {
-    const newItem = {
+  const addForm = (item) => {
+    const newData = {
       id: uuidv4(),
       name: item.name,
       viewers: item.viewers,
       favourite: false,
       like: false,
     };
-    this.setState(({ data }) => ({
-      data: [...data, newItem],
-    }));
+    setData([...data, newData]);
   };
 
-  onToggleProp = (id, prop) => {
-    this.setState(({ data }) => ({
-      data: data.map((item) => {
+  const onToggleProp = (id, prop) => {
+    setData(
+      data.map((item) => {
         if (item.id === id) return { ...item, [prop]: !item[prop] };
         return item;
-      }),
-    }));
+      })
+    );
   };
 
-  searchHandler = (data, term) => {
+  const searchHandler = (data, term) => {
     if (term.length === 0) return data;
     return data.filter((item) => item.name.toLowerCase().indexOf(term) > -1);
   };
 
-  filterHandler = (data, filter) => {
+  const filterHandler = (data, filter) => {
     switch (filter) {
       case "popular":
         return data.filter((c) => c.like);
       case "mostViewers":
-        return data.filter((c) => c.viewers > 800);
+        return data.filter((c) => c.viewers > 10000);
       default:
         return data;
     }
   };
 
-  updateTermHandler = (term) => this.setState({ term });
+  const updateTermHandler = (term) => setTerm(term);
+  const updateFilterHandler = (filter) => setFilter(filter);
 
-  updateFilterHandler = (filter) => this.setState({ filter });
-
-  render() {
-    const { data, term, filter } = this.state;
-    const allMovieCount = data.length;
-    const allFavouriteMovieCount = data.filter((c) => c.favourite).length;
-    const visibleData = this.filterHandler(
-      this.searchHandler(data, term),
-      filter
-    );
-
-    return (
-      <div className="app font-monospace">
-        <div className="content">
-          <AppInfo
-            allMovieCount={allMovieCount}
-            allFavouriteMovieCount={allFavouriteMovieCount}
+  return (
+    <div className="app font-monospace">
+      <div className="content">
+        <AppInfo
+          allMovieCount={data.length}
+          allFavouriteMovieCount={data.filter((c) => c.favourite).length}
+        />
+        <div className="search-panel">
+          <SearchPanel updateTermHandler={updateTermHandler} />
+          <AppFilter
+            filter={filter}
+            updateFilterHandler={updateFilterHandler}
           />
-          <div className="search-panel">
-            <SearchPanel updateTermHandler={this.updateTermHandler} />
-            <AppFilter
-              filter={filter}
-              updateFilterHandler={this.updateFilterHandler}
-            />
-          </div>
-          <MovieList
-            onToggleProp={this.onToggleProp}
-            data={visibleData}
-            onDelete={this.onDelete}
-          />
-          <MoviesAddForm addForm={this.addForm} />
         </div>
+        <MovieList
+          onToggleProp={onToggleProp}
+          data={filterHandler(searchHandler(data, term), filter)}
+          onDelete={onDelete}
+        />
+        <MoviesAddForm addForm={addForm} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,55 +1,47 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./movies-add-form.css";
 
-class MoviesAddForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      views: "",
-    };
-  }
+const MoviesAddForm = ({ addForm }) => {
+  const [state, setState] = useState({ name: "", views: "" });
 
-  changeHandlerInput = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const changeHandlerInput = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  addFormHandler = (e) => {
+  const addFormHandler = (e) => {
     e.preventDefault();
-    this.props.addForm({ name: this.state.name, viewers: this.state.views });
-    this.setState({ name: "", views: "" });
+    if (state.name === "" || state.views === "") return;
+    addForm({ name: state.name, viewers: state.views });
+    setState({ name: "", views: "" });
   };
 
-  render() {
-    const { name, views } = this.state;
-    return (
-      <div className="app-add-form">
-        <h3>Yangi kino qo'shish</h3>
-        <form className="add-form d-flex" onSubmit={this.addFormHandler}>
-          <input
-            type="text"
-            className="form-control new-post-label"
-            placeholder="Qanday kino?"
-            name="name"
-            value={name}
-            onChange={this.changeHandlerInput}
-          />
-          <input
-            type="number"
-            className="form-control new-post-label"
-            placeholder="Nechi marotaba ko'rilgan?"
-            name="views"
-            value={views}
-            onChange={this.changeHandlerInput}
-          />
+  return (
+    <div className="app-add-form">
+      <h3>Yangi kino qo'shish</h3>
+      <form className="add-form d-flex" onSubmit={addFormHandler}>
+        <input
+          type="text"
+          className="form-control new-post-label"
+          placeholder="Qanday kino?"
+          name="name"
+          value={state.name}
+          onChange={changeHandlerInput}
+        />
+        <input
+          type="number"
+          className="form-control new-post-label"
+          placeholder="Nechi marotaba ko'rilgan?"
+          name="views"
+          value={state.views}
+          onChange={changeHandlerInput}
+        />
 
-          <button type="submit" className="btn btn-outline-dark">
-            Qo'shish
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <button type="submit" className="btn btn-outline-dark">
+          Qo'shish
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default MoviesAddForm;
